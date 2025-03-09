1a2e692542f854150928db5665a6dc5d0c57096f
fquinteroc <f.quinteroc@uniandes.edu.co>
2025-03-08 21:19:53 -0500

    Login MediTime - Web

---
+++ meditime-web/src/app/auth/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+
+  login() {
+    alert(`Usuario: ${this.username}\nContraseña: ${this.password}\nRecordar: ${this.rememberMe}`);
+  }
+}
