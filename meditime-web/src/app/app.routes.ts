fe9db2acc0968f6b0caf9f00fa0df4134bf0ad8c
dannamilenacoral <d.corall@uniandes.edu.co>
2025-03-13 15:42:42 -0500

    Archivo app.routes update

--- meditime-web/src/app/app.routes.ts
+++ meditime-web/src/app/app.routes.ts
@@ -5,30 +5,46 @@ import { ConfirmResetComponent } from './auth/confirm-reset/confirm-reset.compon
 import { NewPasswordComponent } from './auth/new-password/new-password.component';
 import { NewAccountComponent } from './auth/new-account/new-account.component';
 import { PatientsListComponent } from './dashboard/patients-list/patients-list.component';
+import { PatientDetailComponent } from './dashboard/patient-detail/patient-detail.component';
+import { MedicationListComponent } from './dashboard/medication-list/medication-list.component';
+import { RegisterMedicationComponent } from './dashboard/register-medication/register-medication.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LoginComponent
-    },
-    {
-        path: 'reset-password',
-        component: ResetPasswordComponent
-    },
-    {
-        path: 'confirm-reset',
-        component: ConfirmResetComponent
-    },
-    {
-        path: 'new-password',
-        component: NewPasswordComponent
-    },
-    {
-        path: 'new-account',
-        component: NewAccountComponent
-    },
-    {
-        path: 'patients-list',
-        component: PatientsListComponent
-    }
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent
+  },
+  {
+    path: 'confirm-reset',
+    component: ConfirmResetComponent
+  },
+  {
+    path: 'new-password',
+    component: NewPasswordComponent
+  },
+  {
+    path: 'new-account',
+    component: NewAccountComponent
+  },
+  {
+    path: 'patients-list',
+    component: PatientsListComponent
+  },
+  {
+    path: 'patient-detail',
+    component: PatientDetailComponent
+  },
+  {
+    path: 'medication-list',
+    component: MedicationListComponent
+  },
+  {
+    path: 'register-medication',
+    component: RegisterMedicationComponent
+  }
 ];
+
