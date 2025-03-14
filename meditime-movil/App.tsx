bbb3f4815f9be3d726f33d46612636395a825269
dannamilenacoral <d.corall@uniandes.edu.co>
2025-03-14 11:24:02 -0500

    Ajustes en app.tsx

--- meditime-movil/App.tsx
+++ meditime-movil/App.tsx
@@ -1,58 +1,52 @@
-import {StatusBar} from 'expo-status-bar';
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import {createStaticNavigation} from '@react-navigation/native';
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {ScreenContent} from './components/ScreenContent';
-import {Settings} from "./components/Settings";
-import {Reports} from "./components/Reports";
-import {History} from "./components/History";
-import {SignIn} from "./components/SignIn";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { History } from './components/History';
+import { Reports } from './components/Reports';
+import { ScreenContent } from './components/ScreenContent';
+import { Settings } from './components/Settings';
+import { SignIn } from './components/SignIn';
 
 import './global.css';
 
-const RootStack = createNativeStackNavigator({
-    initialRouteName: 'SignIn',
-    screens: {
-        SignIn: {
-            screen: SignIn,
-            options: {
-                headerShown: false,
-            },
-        },
-        Home: {
-            screen: ScreenContent,
-            options: {
-                headerShown: false,
-            },
-        },
-        Settings: {
-            screen: Settings,
-            options: {
-                headerShown: false,
-            },
-        },
-        Reports: {
-            screen: Reports,
-            options: {
-                headerShown: false,
-            },
-        },
-        History: {
-            screen: History,
-            options: {
-                headerShown: false,
-            },
-        }
-    },
-});
-
-const Navigation = createStaticNavigation(RootStack);
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <SafeAreaProvider>
-            <Navigation/>
-            <StatusBar style="auto"/>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="SignIn">
+                    <Stack.Screen
+                        name="SignIn"
+                        component={SignIn}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="Home"
+                        component={ScreenContent}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="Settings"
+                        component={Settings}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="Reports"
+                        component={Reports}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="History"
+                        component={History}
+                        options={{ headerShown: false }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+            <StatusBar style="auto" />
         </SafeAreaProvider>
     );
 }
