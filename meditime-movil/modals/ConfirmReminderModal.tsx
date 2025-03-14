cb8b9d295e60cd16d6d377dc251233a8ed93234c
dannamilenacoral <d.corall@uniandes.edu.co>
2025-03-14 08:53:39 -0500

    Ajustes finales movil modal confirmar 2

--- meditime-movil/modals/ConfirmReminderModal.tsx
+++ meditime-movil/modals/ConfirmReminderModal.tsx
@@ -1,41 +1,40 @@
-import {Button, Modal, Pressable, Text, View} from "react-native";
-import {X} from "lucide-react-native";
+import { Button, Modal, Pressable, Text, View } from 'react-native';
+import { X } from 'lucide-react-native';
 
 type Props = {
-    isOpen: boolean,
-    onClose: () => void,
-    onConfirm?: () => void,
-}
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm?: () => void;
+};
 
-export function ConfirmReminderModal({isOpen, onConfirm, onClose}: Props) {
-    return (
-      <Modal animationType="slide" transparent={true} visible={isOpen} onRequestClose={onClose}>
-        <View className="flex-1 items-center justify-center">
-          <View className="relative w-9/12 gap-4 rounded bg-white p-12 shadow-md">
-            <Pressable className="absolute right-5 top-5" onPress={onClose}>
-              <X size={16} color="black" />
-            </Pressable>
-            <Text>Tomaste tu medicación de:</Text>
-            <Text className="text-center text-lg font-bold">Aspirina</Text>
+export function ConfirmReminderModal({ isOpen, onConfirm, onClose }: Props) {
+  return (
+    <Modal animationType="slide" transparent={true} visible={isOpen} onRequestClose={onClose}>
+      <View className="flex-1 items-center justify-center">
+        <View className="relative w-9/12 gap-4 rounded bg-white p-12 shadow-md">
+          <Pressable className="absolute right-5 top-5" onPress={onClose}>
+            <X size={16} color="black" />
+          </Pressable>
+          <Text>Tomaste tu medicación de:</Text>
+          <Text className="text-center text-lg font-bold">Aspirina</Text>
 
-            <View className="flex-row gap-4">
-              <View className="flex-1">
-                <Button
-                  onPress={() => {
-                      onConfirm();
-                    onClose();
-                  }}
-                  color="green"
-                  title="Si"
-                />
-              </View>
-              <View className="flex-1">
-                <Button onPress={onClose} color="gray" title="No" />
-              </View>
+          <View className="flex-row gap-4">
+            <View className="flex-1">
+              <Button
+                onPress={() => {
+                  onConfirm?.();
+                  onClose();
+                }}
+                color="green"
+                title="Si"
+              />
+            </View>
+            <View className="flex-1">
+              <Button onPress={onClose} color="gray" title="No" />
             </View>
           </View>
         </View>
-      </Modal>
-    );
+      </View>
+    </Modal>
+  );
 }
-
